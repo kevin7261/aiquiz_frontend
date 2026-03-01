7dccd46476ce1cb6ba8d999de231d7a2da225045
kevin7261 <kevin7261@github.com>
2026-03-02 02:13:44 +0800

    Update API_BASE constant to include commented local development URL for easier testing

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,5 @@
 /**
  * 後端 API 基底網址，全專案統一由此設定。
  */
-export const API_BASE = 'https://aiquiz-backend-z4mo.onrender.com';
+export const API_BASE = 'https://aiquiz-backend-z4mo.onrender.com';//'http://127.0.0.1:8000';
+
